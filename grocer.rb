996a8cf0c6f2dad4b78c99b89fcf5baad1f95cc9
Paul Clark <clark.paul123@gmail.com>
2018-10-25 17:02:51 +0000

    Done.

--- grocer.rb
+++ grocer.rb
@@ -5,8 +5,8 @@ def consolidate_cart(cart)
   cart.each do |item_hash|
     item_hash.each do |item, attributes|
       if consolidated_cart.include?(item)
-        consolidated_cart[item][:count] += 1 
-      else 
+        consolidated_cart[item][:count] += 1
+      else
         consolidated_cart[item] = attributes
         consolidated_cart[item][:count] = 1
       end
@@ -18,19 +18,19 @@ end
 def apply_coupons(cart, coupons)
   coupons.each do |coupon|
     item_name = coupon[:item]
-    
+
     # `if cart[item_name]` is in case coupon doesn't apply
-    
+
     if cart[item_name] && cart[item_name][:count] >= coupon[:num]
       cart[item_name][:count] -= coupon[:num]
       if
         cart["#{item_name} W/COUPON"]
         cart["#{item_name} W/COUPON"][:count] +=1
-      else  
-        
+      else
+
         # working backwards from here - the `else` adds to the hash unless key
         # already exists
-        
+
         cart["#{item_name} W/COUPON"] = {count: 1, price: coupon[:cost], clearance: cart[item_name][:clearance]}
       end
     end
@@ -39,29 +39,37 @@ def apply_coupons(cart, coupons)
 end
 
 # would above would work if coupon count and item count identical?
-# would give this result when 2 avocados in the cart: 
+# would give this result when 2 avocados in the cart:
 #
 #   COUPON {:item => "AVOCADO", :num => 2, :cost => 5.0}
 #
 #   CART {"AVOCADO" => {:price => 3.0, :clearance => true, :count
-#   => 0}, "AVOCADO W/COUPON" => {:price => 5.0, :clearance => 
-#   true, :count => 1}} 
+#   => 0}, "AVOCADO W/COUPON" => {:price => 5.0, :clearance =>
+#   true, :count => 1}}
 #
 
 # apply_clearance takes 20% off price if the item is on clearance
 
 def apply_clearance(cart)
-  cart.map do |item_name, attributes|
-    if cart[item_name][:clearance]
-      
-      #should be a way to combine next 2 lines
-      cart[item_name][:price] *= 0.8
-      cart[item_name][:price] = cart[item_name][:price].round(2)
-    end  
+  cart.map do |item_name, data|
+    if data[:clearance]
+      data[:price] = (data[:price] * 0.8).round(2)
+    end
   end
   cart
 end
 
 def checkout(cart, coupons)
+  price = 0
+  consolidated_cart = consolidate_cart(cart)
+  apply_coupons(consolidated_cart, coupons)
+  apply_clearance(consolidated_cart)
 
+  consolidated_cart.values.each do |item|
+    price += (item[:price] * item[:count])
+  end
+  if price >= 100
+    price = (price * 0.9)
+  end
+  price
 end
